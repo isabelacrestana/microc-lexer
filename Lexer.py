97c1b5fff389e42676f5159650acad03eda2b2f4
PG2zinho <pedro-gasparotto@outlook.com>
2026-08-26 09:33:44 -0300

    First setup of the lexer

--- Lexer.py
+++ Lexer.py
@@ -81,6 +81,137 @@ class Lexer:
     def __init__(self, source: str):
         self.source = source
         # TODO: inicialize aqui o estado exigido por sua estratégia.
+        self.position = 0
+        self.length = len(source)
+
+        self.transition_table = {
+
+            'start': {
+                'alpha': self.state_identifier,
+                'digit': self.state_number,
+                '+': self.state_simple_token,
+                '-': self.state_simple_token,
+                '/': self.state_slash,
+                '*': self.state_simple_token,
+                '%': self.state_simple_token,
+                '<': self.state_less,
+                '>': self.state_greater,
+                '=': self.state_assign,
+                '!': self.state_not,
+                '&': self.state_and,
+                '|': self.state_or,
+                '(': self.state_simple_token,
+                ')': self.state_simple_token,
+                '"': self.state_string,
+                '{': self.state_simple_token,
+                '}': self.state_simple_token,
+                ',': self.state_simple_token,
+                ';': self.state_simple_token,
+                'whitespace': self.state_whitespace,
+                'EOF': self.state_EOF,
+                },
+
+            'identifier':{
+                'alpha': self.state_identifier,
+                'digit': self.state_identifier,
+                'EOF': self.state_accept_identifier,
+                'default': self.state_accept_identifier,
+            },
+
+            'digit': {
+                'digit': self.state_number,
+                'alpha': self.state_invalid,
+                'EOF': self.state_accept_number,
+                'default': self.state_accept_number,
+            },
+
+            'slash':{
+                '/': self.state_comment_line,
+                '*': self.state_comment_block,
+                'EOF': self.state_accept,
+                'default': self.state_accept,
+            },
+
+            'assign': {
+                '=': self.state_equal,
+                'EOF': self.state_accept,
+                'default': self.state_accept,
+            },
+
+            'less':{
+                '=': self.state_less_equal,
+                'EOF': self.state_accept,
+                'default': self.state_accept,
+            },
+
+            'greater':{
+                '=': self.state_greater_equal,
+                'EOF': self.state_accept,
+                'default': self.state_accept,
+            },
+
+            'NOT':{
+                '=': self.state_not_equal,
+                'EOF': self.state_accept,
+                'default': self.state_accept
+            },
+
+            'AND':{
+                '&': self.state_accept,
+                'EOF': self.state_invalid,
+                'default': self.state_invalid,
+            },
+
+            'OR':{
+                '|': self.state_accept,
+                'EOF': self.state_invalid,
+                'default': self.state_invalid,
+            }   
+        }
+
+        self.simple_tokens = {
+            '+': TokenKind.PLUS,
+            '-': TokenKind.MINUS,
+            '*': TokenKind.STAR,
+            '%': TokenKind.PERCENT,
+            '(': TokenKind.LEFT_PAREN,
+            ')': TokenKind.RIGHT_PAREN,
+            '{': TokenKind.LEFT_BRACE,
+            '}': TokenKind.RIGHT_BRACE,
+            ',': TokenKind.COMMA,
+            ';': TokenKind.SEMICOLON
+        }
+
+        self.keywords = {
+            "int": TokenKind.KW_INT,
+            "bool": TokenKind.KW_BOOL,
+            "void": TokenKind.KW_VOID,
+            "true": TokenKind.KW_TRUE,
+            "false": TokenKind.KW_FALSE,
+            "if": TokenKind.KW_IF,
+            "else": TokenKind.KW_ELSE,
+            "while": TokenKind.KW_WHILE,
+            "return": TokenKind.KW_RETURN,
+            "print": TokenKind.KW_PRINT
+        }
+
+    def state_simple_token(self) -> Token:
+        inicial_line = self.line
+        inicial_column = self.column
+
+        char = self.source[self.position]
+
+        self.column += 1
+        self.position += 1
+
+        kind = self.simple_tokens[char]
+
+        return Token(kind, char, None, inicial_line, inicial_column)
+
+    def state_whitespace(self) -> None:
+
+        return None
+
 
     def tokens(self) -> Iterator[Token]:
         """Produza todos os tokens significativos e um único EOF ao final."""
@@ -88,5 +219,13 @@ class Lexer:
         yield  # mantém este método como gerador durante o desenvolvimento
 
     def scan(self) -> list[Token]:
+
+        while self.position > len(self.source):
+
+            char = self.source[self.position]
+
+            if char.isspace():
+                self.state_whitespace(self)
+            
         return list(self.tokens())
 
